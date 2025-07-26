77a3b7e5a96b8524d742f49a50b8ef5e4a77d4a9
J-Eugenio <eugeniopb4@gmail.com>
2025-07-26 12:14:47 -0300

    ipgrade-sdk

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,8 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from './pages/Home';
-import Webview from './pages/WebView';
+import Webview from './Pages/WebView';
+import Home from './Pages/Home';
 
 export default function Main() {
   const Stack = createNativeStackNavigator();
